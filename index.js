08027bec4a14c3adcb4aae0464316cf697e6270d
Paul Fd <paul@ferrand.cc>
2021-12-01 01:12:44 +0100

    functional kb

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ class DemoApp {
     this._toggleState = false;
     this._sfzText = null;
     this._ctrlDown = false;
+    this._noteStates = new Array(128);
+    for (let i = 0; i < 128; i++)
+      this._noteStates[i] = { midiState: false, mouseState: false, keyboardState: false, element: undefined, whiteKey: undefined };
   }
 
   _initializeView() {
     this._container = document.getElementById('demo-app');
     this._toggleButton = document.getElementById('audio-toggle');
-    this._toggleButton.addEventListener(
-      'mouseup', () => this._handleToggle());
+    this._toggleButton.addEventListener('mouseup', () => this._handleToggle());
     this._sfzText = document.getElementById('sfz-text');
     this._reloadButton = document.getElementById('reload-text');
     this._reloadButton.addEventListener(
@@ -39,7 +41,7 @@ class DemoApp {
 
   _setupWebMidi() {
     if (navigator.requestMIDIAccess) {
-      navigator.requestMIDIAccess({name: "midi", sysex: false})
+      navigator.requestMIDIAccess({ name: "midi", sysex: false })
         .then((midiAccess) => {
           console.log(midiAccess);
           var inputs = midiAccess.inputs;
@@ -60,13 +62,13 @@ class DemoApp {
 
   _labelToNoteNumber(label) {
     const noteNames = ["c", "cs", "d", "ds", "e", "f", "fs", "g", "gs", "a", "as", "b"];
-    const octave =  parseInt(label.length > 3 ? label.slice(-2) : label.slice(-1));
+    const octave = parseInt(label.length > 3 ? label.slice(-2) : label.slice(-1));
     const noteName = label.slice(0, -1);
     const noteIdx = noteNames.findIndex((x) => noteName == x);
     if (noteIdx < 0 || octave < -1)
       return -1;
-      
-    
+
+
     return (octave + 1) * 12 + noteIdx;
   }
 
@@ -93,15 +95,41 @@ class DemoApp {
     var value = (message.data.length > 2) ? message.data[2] / 127.0 : 0; // a velocity value might not be included with a noteOff command
 
     switch (command) {
-      case 144: // noteOn
-        value > 0 ? this._noteOn(number, value) : this._noteOff(number);
+      case 144: { // noteOn
+          const state = this._noteStates[number];
+          if (value > 0) {
+            if (!state.mouseState && !state.keyboardState)
+              this._noteOn(number, value);
+
+            this._noteStates[number].midiState = true;
+          } else {
+            if (!state.mouseState && !state.keyboardState)
+              this._noteOff(number, value);
+
+            this._noteStates[number].midiState = false;
+          }
+          this._updateKeyboardState(number);
+        }
         break;
-      case 128: // noteOff
-        this._noteOff(number)
+      case 128: { // noteOff
+          const state = this._noteStates[number];
+          if (!state.mouseState && !state.keyboardState)
+            this._noteOff(number, value);
+
+          this._noteStates[number].midiState = false;
+          this._updateKeyboardState(number);
+        }
         break;
-      case 224:
-        console.log(message.data[2], message.data[2] << 7, message.data[1], message.data[2] << 7 + message.data[1] << 1 >> 1);
-        // console.log((message.data[1] << 7 + message.data[2]) / 65535 - 0.5);
+      case 176: // cc
+        this._post({ type: 'cc', number: number, value: value });
+        break;
+      case 208: // cc
+        this._post({ type: 'aftertouch', value: value });
+        break;
+      case 224: // pitch wheel
+        var pitch = (((message.data[2] << 7) + message.data[1]) - 8192);
+        pitch = Math.max(-8191, Math.min(8191, pitch)) / 8191.0;
+        this._post({ type: 'pitch_wheel', value: pitch });
         break;
     }
   }
@@ -115,7 +143,6 @@ class DemoApp {
         .connect(this._context.destination);
 
       this._toggleButton.classList.remove(['disabled', 'loading']);
-      this._toneButton.classList.remove('disabled');
       this._reloadButton.classList.remove('disabled');
       this._handleToggle();
     });
@@ -140,6 +167,21 @@ class DemoApp {
     this._synthNode.port.postMessage(message);
   }
 
+  _updateKeyboardState(note) {
+    const state = this._noteStates[note];
+    if (!state.element)
+      return;
+
+    if (!state.midiState && !state.mouseState && !state.keyboardState) {
+      if (state.whiteKey == true)
+        this._noteStates[note].element.style.backgroundColor = 'white';
+      else if (state.whiteKey == false)
+        this._noteStates[note].element.style.backgroundColor = 'black';
+    } else {
+      this._noteStates[note].element.style.backgroundColor = 'steelblue';
+    }
+  }
+
   _buildKeyboard() {
     document.getElementById('keyboard-prototype').hidden = false;
     var whitePrototype = document.getElementById('white-prototype');
@@ -159,21 +201,61 @@ class DemoApp {
       var key = document.createElement('button');
       key.className = cls;
       key.style = 'left: ' + pos + 'px;';
-      key.id = id; 
-      whiteCount += (cls == 'white');
+      key.id = id;
       const note = this._labelToNoteNumber(id);
+      this._noteStates[note].element = key;
+
+      if (cls == 'white') {
+        whiteCount += 1;
+        this._noteStates[note].whiteKey = true;
+      } else {
+        this._noteStates[note].whiteKey = false;
+      }
+
       if (note >= 0) {
         const keyHeight = (cls == 'white') ? whiteKeyHeight : blackKeyHeight;
-        key.addEventListener('mousedown', (e) => this._noteOn(note, e.offsetY / keyHeight));
-        key.addEventListener('mouseup', () => this._noteOff(note));        
+        key.addEventListener('mousedown', (e) => {
+          const state = this._noteStates[note];
+          if (!state.midiState 
+              && !state.keyboardState)
+            this._noteOn(note, e.offsetY / keyHeight);
+
+          this._noteStates[note].mouseState = true;
+          this._updateKeyboardState(note);
+        });
+        key.addEventListener('mouseup', () => {
+          const state = this._noteStates[note];
+          if (!state.midiState && !state.keyboardState)
+            this._noteOff(note);
+            
+          this._noteStates[note].mouseState = false;
+          this._updateKeyboardState(note);
+        });
+        key.addEventListener('mouseleave', () => {
+          const state = this._noteStates[note];
+          if (state.mouseState && !state.midiState && !state.keyboardState)
+            this._noteOff(note);
+            
+          this._noteStates[note].mouseState = false;
+          this._updateKeyboardState(note);
+        });
+        key.addEventListener('mouseenter', (e) => {
+          const state = this._noteStates[note];
+          if ((e.buttons & 1) && !state.midiState && !state.keyboardState) {
+            this._noteOn(note, e.offsetY / keyHeight);
+            
+            this._noteStates[note].mouseState = true;
+          }
+          this._updateKeyboardState(note);
+        });
       }
       this._keyboard.appendChild(key);
     };
 
     var makeOctave = (shift, octaveNumber) => {
-      keyNames.forEach((keyName, idx) => 
+      keyNames.forEach((keyName, idx) =>
         makeKey(shift + idx * whiteKeyWidth, keyName + octaveNumber, 'white'));
-      blackPositions.forEach((whiteIdx) => 
+      blackPositions.forEach((whiteIdx) =>
         makeKey(shift + whiteIdx * whiteKeyWidth + blackOffset, keyNames[whiteIdx] + 's' + octaveNumber, 'black'));
     };
 
